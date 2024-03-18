b5a76659b37f7e39bf775bf54a38d063e4531b04
thirstyplastic <114196215+thirstyplastic@users.noreply.github.com>
2024-03-18 15:45:29 -0400

    added new read_time function

--- functions.py
+++ functions.py
@@ -2,9 +2,13 @@ import requests
 
 api_key = 'VEpYM62bVimeAcNhoXDgpE9GRGo9OBFF'
 # MAKES API CALL
-def class_api_call(class_number,api_key=api_key):
-    url_base = 'https://sis.jhu.edu/api/classes/{course_number}/{term}?key='.format(course_number=class_number,term='Fall 2023') + api_key
+def class_api_call(class_number,term,api_key=api_key):
+    url_base = 'https://sis.jhu.edu/api/classes/{course_number}/{term}?key='.format(course_number=class_number,term=term) + api_key
     return requests.get(url_base).json()
 
+def read_time():
+    print('FIXME: should read a time')
+    return 0
+
 if __name__ == '__main__':
-    print(class_api_call('AS001132'))
+    print(class_api_call('AS001132', 'Fall 2024'))
