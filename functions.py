1cb245c39d78f06d1a810f7ac7d0f85a959bbe68
thirstyplastic <114196215+thirstyplastic@users.noreply.github.com>
2024-03-18 15:30:24 -0400

    added api call function

--- functions.py
+++ functions.py
@@ -1,2 +1,10 @@
-import classes
-import functions
+import requests
+
+api_key = 'VEpYM62bVimeAcNhoXDgpE9GRGo9OBFF'
+# MAKES API CALL
+def class_api_call(class_number,api_key=api_key):
+    url_base = 'https://sis.jhu.edu/api/classes/{course_number}/{term}?key='.format(course_number=class_number,term='Fall 2023') + api_key
+    return requests.get(url_base).json()
+
+if __name__ == '__main__':
+    print(class_api_call('AS001132'))
--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-print("hello world")
+import classes
+import functions
