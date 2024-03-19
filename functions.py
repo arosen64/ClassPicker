31ba308053641933eae149c9fd0e366d10d6d882
thirstyplastic <114196215+thirstyplastic@users.noreply.github.com>
2024-03-19 02:39:31 -0400

    fixed time calls so the meetings are much more readable

--- functions.py
+++ functions.py
@@ -4,16 +4,20 @@ import json
 
 # MAKES API CALL
 def class_api_call(class_number,term,api_key):
-    url_base = 'https://sis.jhu.edu/api/classes/{course_number}/{term}?key='.format(course_number=class_number,term=term) + api_key
+    url_base = f'https://sis.jhu.edu/api/classes/{class_number}/{term}?key=' + api_key
+    return requests.get(url_base).json()
+
+def time_call(class_number,section_num,DOW,term,api_key):
+    url_base = f'https://sis.jhu.edu/api/classes?key={api_key}&CourseNumber={class_number}{section_num}&DaysOfWeek=any|{DOW}&Term={term}'
     return requests.get(url_base).json()
 
 def read_time():
     print('FIXME: should read a time')
     return 0
 
-def write_data(data_dict):
+def write_data(data_dict,filename):
     json_object = json.dumps(data_dict, indent=4)
-    with open("data.json", "w") as outfile:
+    with open(filename, "w") as outfile:
         outfile.write(json_object)
         
 def read_from_file(file_name):
@@ -34,13 +38,17 @@ def read_from_web(data_dict,term,api_key):
     copy_dict = data_dict
     for key in copy_dict:
         copy_dict[key] = class_api_call(key,term,api_key)
+        for section in copy_dict[key]:
+            add = (time_call(key,section['SectionName'],section['DOW'],term,api_key))
+            if len(add):
+                section['Meetings'] = add[0]['SectionDetails'][0]['Meetings']
     return copy_dict
 
 if __name__ == '__main__':
     api_key = 'VEpYM62bVimeAcNhoXDgpE9GRGo9OBFF'
     # print(class_api_call('AS110109', 'Spring 2024', api_key))
     class_data = {'AS110109':[], 'AS180242':[], 'EN601220':[], 'EN601230':[]}
-    # read_from_web(class_data,'Spring 2024', api_key)
+    read_from_web(class_data,'Spring 2024', api_key)
     # write_data(class_data)
     # class_data = read_from_file()
-    # print(class_data)
+    #print(class_data)
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ get_mode = True
 #get/load data based on download mode
 if get_mode:
     class_data = read_from_web(class_data,term,api_key)
+    write_data(class_data,'data.json')
 else:
     class_data = read_from_file('data.json')
 
