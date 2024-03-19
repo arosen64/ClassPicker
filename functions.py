80bad80039f0ff9897e63a58b96238fb1787a745
thirstyplastic <114196215+thirstyplastic@users.noreply.github.com>
2024-03-19 16:53:00 -0400

    added recursive function that creates all possible combinations of classes

--- functions.py
+++ functions.py
@@ -44,11 +44,26 @@ def read_from_web(data_dict,term,api_key):
                 section['Meetings'] = add[0]['SectionDetails'][0]['Meetings']
     return copy_dict
 
+def all_variations(lst, index):
+    if index < 0:
+        return [[]]
+
+    result = []
+    for variation in all_variations(lst, index - 1):
+        for item in lst[index]:
+            result.append(variation + [item])
+
+    return result
+    
+
 if __name__ == '__main__':
     api_key = 'VEpYM62bVimeAcNhoXDgpE9GRGo9OBFF'
     # print(class_api_call('AS110109', 'Spring 2024', api_key))
-    class_data = {'AS110109':[], 'AS180242':[], 'EN601220':[], 'EN601230':[]}
-    read_from_web(class_data,'Spring 2024', api_key)
+    #class_data = {'AS110109':[], 'AS180242':[], 'EN601220':[], 'EN601230':[]}
+    #read_from_web(class_data,'Spring 2024', api_key)
     # write_data(class_data)
     # class_data = read_from_file()
     #print(class_data)
+    data = [[{'A':[]},{'B':[]},{'C':[]}],[{'1':[]},{'2':[]},{'3':[]}],[{'&':[]},{'@':[]},{'$':[]}]]
+    
+    print(len(all_variations(data,len(data)-1)))
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from functions import *
 #classes info
 api_key = 'VEpYM62bVimeAcNhoXDgpE9GRGo9OBFF'
 term = 'Spring 2024'
-class_data = {'AS110109':[], 'AS110110':[], 'AS180242':[], 'EN601220':[], 'EN601230':[]}
-combos= [('AS110109','AS110110')]
+class_data = {'AS110109':[], 'EN660203':[], 'AS180242':[], 'EN601220':[], 'EN601230':[]}
+combos= [('AS110109','EN660203')]
 
 #downloading data?
 get_mode = False
@@ -27,5 +27,16 @@ if len(combos):
             del class_data[class_num]
             combo_string += class_num + '-'
         class_data[combo_string] = new_data
+
+#putting all data into a list
+all_schedules = []
+
+for key in class_data:
+    all_schedules.append(class_data[key])
+
+#creating every variation of the classes
+all_schedules = all_variations(all_schedules, len(all_schedules)-1)
+
+
 
 
