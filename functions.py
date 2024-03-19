99416c89feb86cf09df413bc714c448bbe4659fb
thirstyplastic <114196215+thirstyplastic@users.noreply.github.com>
2024-03-18 23:01:42 -0400

    added helper functions for reading to and from json files

--- functions.py
+++ functions.py
@@ -1,8 +1,9 @@
 import requests
+import json
 
-api_key = 'VEpYM62bVimeAcNhoXDgpE9GRGo9OBFF'
+
 # MAKES API CALL
-def class_api_call(class_number,term,api_key=api_key):
+def class_api_call(class_number,term,api_key):
     url_base = 'https://sis.jhu.edu/api/classes/{course_number}/{term}?key='.format(course_number=class_number,term=term) + api_key
     return requests.get(url_base).json()
 
@@ -10,5 +11,22 @@ def read_time():
     print('FIXME: should read a time')
     return 0
 
+def write_data(data_dict):
+    json_object = json.dumps(data_dict, indent=4)
+    with open("data.json", "w") as outfile:
+        outfile.write(json_object)
+        
+def read_from_file():
+    print('FIXME: should read class data from json file')
+    return 0
+
+def read_from_web(data_dict,term,api_key):
+    for key in data_dict:
+        data_dict[key] = class_api_call(key,term,api_key)
+
 if __name__ == '__main__':
-    print(class_api_call('AS001132', 'Fall 2024'))
+    api_key = 'VEpYM62bVimeAcNhoXDgpE9GRGo9OBFF'
+    # print(class_api_call('AS110109', 'Spring 2024', api_key))
+    # class_data = {'AS110109':[], 'AS180242':[], 'EN601220':[], 'EN601230':[]}
+    # read_from_web(class_data,'Spring 2024', api_key)
+    # write_data(class_data)
--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import classes
 import functions
+
+#classes info
+api_key = 'VEpYM62bVimeAcNhoXDgpE9GRGo9OBFF'
+term = 'Spring 2024'
+class_data = {'AS110109':[], 'AS180242':[], 'EN601220':[], 'EN601230':[]}
+
+#downloading data 
+get_mode = True
+
+#get/load data based on download mode
+
+
