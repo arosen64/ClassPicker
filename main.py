cb99d97105cbd3e9e5b21385b2a259825acfe67b
thirstyplastic <114196215+thirstyplastic@users.noreply.github.com>
2024-03-19 04:24:10 -0400

    made it so that classes can be interchanged for each other

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from functions import *
 #classes info
 api_key = 'VEpYM62bVimeAcNhoXDgpE9GRGo9OBFF'
 term = 'Spring 2024'
-class_data = {'AS110109':[], 'AS180242':[], 'EN601220':[], 'EN601230':[]}
+class_data = {'AS110109':[], 'AS110110':[], 'AS180242':[], 'EN601220':[], 'EN601230':[]}
+combos= [('AS110109','AS110110')]
 
 #downloading data?
-get_mode = True
+get_mode = False
 
 #get/load data based on download mode
 if get_mode:
@@ -15,4 +16,16 @@ if get_mode:
     write_data(class_data,'data.json')
 else:
     class_data = read_from_file('data.json')
+
+#combine combo classes
+if len(combos):
+    for combo in combos:
+        new_data = []
+        combo_string = ''
+        for class_num in combo:
+            new_data += class_data[class_num]
+            del class_data[class_num]
+            combo_string += class_num + '-'
+        class_data[combo_string] = new_data
+
 
